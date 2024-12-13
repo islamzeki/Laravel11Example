8786d4d0bbe6e02a4a447f28f8ddf62f4da8001f
Zeki Aksu <islamzeki@islamzeki.com.tr>
2024-12-13 20:50:24 +0300

    Seeder ve Factory added.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Developer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         Developer::factory()->create([
+             'difficulty' => 1,
+         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Developer::factory()->create([
+            'difficulty' => 2,
+        ]);
+
+        Developer::factory()->create([
+            'difficulty' => 3,
+        ]);
+
+        Developer::factory()->create([
+            'difficulty' => 4,
+        ]);
+
+        Developer::factory()->create([
+            'difficulty' => 5,
+        ]);
     }
 }
